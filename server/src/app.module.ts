e309864e8b8f1a5a964adf6855a8abb4c44f9787
ad.popov.it@gmail.com <562023027Ad>
2021-10-27 11:34:56 -0700

    for deploy

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,12 +3,13 @@ import {MongooseModule} from "@nestjs/mongoose";
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {RequestsModule} from './requests-module/requests.module';
-
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     RequestsModule,
-    MongooseModule.forRoot("mongodb+srv://alexp:alexp@cluster0.wtmws.mongodb.net/requests?retryWrites=true&w=majority")],
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(`mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.wtmws.mongodb.net/requests?retryWrites=true&w=majority`)],
   controllers: [AppController],
   providers: [AppService],
 })
