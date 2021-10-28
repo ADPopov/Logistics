9dc03d8c97150fe6c187779a648aef876ad0d84f
ad.popov.it@gmail.com <562023027Ad>
2021-10-28 05:43:38 -0700

    added swagger client

---
+++ README.md
@@ -0,0 +1,29 @@
+
+## Installation client
+
+```bash
+$ cd client
+$ yarn
+```
+
+## Running the client
+
+```bash
+# watch mode
+$ yarn start
+```
+
+
+## Installation server
+
+```bash
+$ cd server
+$ yarn
+```
+
+## Running the server
+
+```bash
+# watch mode
+$ yarn start:dev
+```
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule,{ cors: true });
+
+  const config = new DocumentBuilder()
+  .setTitle('Logistics')
+  .setDescription('The API description')
+  .setVersion('1.0')
+  .addTag('')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(process.env.PORT || 3000 );
 }
 
